1719cdcfab72dcdd884f06723080572318d73ea9
Aleena Tim <aleenatim@Aleenas-MacBook-Air.local>
2024-07-12 20:06:10 -0400

    edit

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,28 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect('ongodb+srv://user:mongodb123!@cluster0.fqyyhas.mongodb.net/');
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log(`MongoDB Connected`);
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log(`Error: ${error.message}`);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
-
-// Use this function in server.js to connect to mongoDB and start the server
-
-// const startServer = async () => {
-//   try {
-//     await connectDB();
-//     app.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// startServer();
